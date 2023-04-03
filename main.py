0e7969bd7e4a6b18892850eea53228cf60c7e35f
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2023-04-04 00:16:19 +0800

    ⚙ FIX: Add function to button

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from kivymd.uix.textfield.textfield import MDTextField
 from kivymd.uix.menu import MDDropdownMenu
 from kivy.uix.widget import Widget
 
+from samudra.startup import on_start
+from samudra.interfaces import LemmaQueryBuilder
+
 class MainApp(MDApp):
     def build(self) -> Widget:
         self.theme_cls.theme_style = "Dark"
@@ -23,5 +26,10 @@ class MainApp(MDApp):
     def menu_callback(self, text):
         print(text)
 
+    def search(self, *args):
+        data = LemmaQueryBuilder(konsep='',lemma=self.root.ids['lemma_field'].text).get_cakupan().get_kata_asing()
+        print(data.collect())
+
 if __name__ == '__main__':
+    on_start()
     MainApp().run()
