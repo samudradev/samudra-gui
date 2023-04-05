9cc85cb2302be4db6f9d0ad03dcbadb1db77a530
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2023-04-05 23:54:51 +0800

    ⚙ FIX: Display data

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from kivymd.app import MDApp
 from kivymd.uix.textfield.textfield import MDTextField
 from kivymd.uix.menu import MDDropdownMenu
 from kivy.uix.widget import Widget
+from kivy.properties import StringProperty
 
 from samudra.startup import on_start
 from samudra.interfaces import LemmaQueryBuilder
 
+
 class MainApp(MDApp):
+    lemma_data_heading = StringProperty()
+    lemma_data_konsep = StringProperty("test")
+    lemma_data_golongan_kata = StringProperty()
+    lemma_data_cakupan = StringProperty()
+
     def build(self) -> Widget:
         self.theme_cls.theme_style = "Dark"
         menu_items = [
@@ -14,12 +21,11 @@ class MainApp(MDApp):
                 "text": i,
                 "viewclass": "OneLineListItem",
                 "on_release": lambda x=f"Item {i}": self.menu_callback(x),
-            } for i in ['edit', 'delete']
+            }
+            for i in ["edit", "delete"]
         ]
         self.menu = MDDropdownMenu(
-            caller=self.root.ids.menu_button,
-            items=menu_items,
-            width_mult=4
+            caller=self.root.ids["menu_button"], items=menu_items, width_mult=4
         )
         return self.root
 
@@ -27,9 +33,21 @@ class MainApp(MDApp):
         print(text)
 
     def search(self, *args):
-        data = LemmaQueryBuilder(konsep='',lemma=self.root.ids['lemma_field'].text).get_cakupan().get_kata_asing()
-        print(data.collect())
+        # TODO Error handling for cakupan and kata_asing if not exist
+        data = (
+            LemmaQueryBuilder(lemma=self.root.ids["lemma_field"].text)
+            .get_cakupan()
+            .get_kata_asing()
+        )
+        if (konsep := data.collect()) is not None:
+            self.lemma_data_heading = konsep.lemma.nama
+            self.lemma_data_golongan_kata = konsep.golongan.nama
+            self.lemma_data_konsep = konsep.keterangan
+            self.lemma_data_cakupan = str(konsep.cakupan.get().cakupan.nama)
+        else:
+            self.lemma_data_konsep = konsep
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     on_start()
     MainApp().run()
