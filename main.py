5af4c74fb98e5a37f1aae1063a3a7829bac81774
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2023-04-15 20:42:54 +0800

    ⚙ FIX: UI

--- main.py
+++ main.py
@@ -17,54 +17,51 @@ class DataCard(MDAnchorLayout):
     lemma: LemmaData = ObjectProperty("", allownone=True)
     anchor_x = "center"
     anchor_y = "center"
+    padding = 25, 25, 25, 25
 
     def build(self) -> Widget:
         self.clear_widgets()
-        box = MDBoxLayout(padding=40)
+        box = MDBoxLayout(padding=30, adaptive_height=True)
         if self.lemma is None:
             box.add_widget(MDLabel(text="Tidak dijumpai"))
             self.add_widget(box)
             return self
-        box.add_widget(MDLabel(text=self.lemma.nama, font_style="H2", underline=True))
-        konsep_box = MDBoxLayout()
+        box.add_widget(MDLabel(text=self.lemma.nama, font_style="H2", underline=True, bold=True))
+        konsep_box = MDBoxLayout(adaptive_height=True, spacing=5)
         for i, konsep in enumerate(self.lemma.konsep, 1):
             konsep_box.add_widget(
                 MDLabel(
                     text="{}. [{:^6}] {}".format(
                         i, konsep.golongan.id, konsep.keterangan
-                    )
+                    ),
                 )
             )
             if len(konsep.kata_asing) > 0:
-                slot__kataasing = MDBoxLayout(orientation="horizontal")
+                slot__kataasing = MDBoxLayout(orientation="horizontal", adaptive_height=True)
                 for kata_asing_connector in konsep.kata_asing:
                     slot__kataasing.add_widget(
                         MDLabel(
-                            text="{}: {}".format(
+                            text="    {}: {}".format(
                                 kata_asing_connector.kata_asing.bahasa,
                                 kata_asing_connector.kata_asing.nama,
-                            )
+                            ),
+                            italic=True
                         )
                     )
+                konsep_box.add_widget(slot__kataasing)
             if len(konsep.cakupan) > 0:
-                slot__cakupan = MDBoxLayout(orientation="horizontal")
+                slot__cakupan = MDBoxLayout(orientation="horizontal", adaptive_size=True)
                 for cakupan_connector in konsep.cakupan:
                     slot__cakupan.add_widget(
                         MDChip(text=cakupan_connector.cakupan.nama, padding=0)
                     )
                 konsep_box.add_widget(slot__cakupan)
-                konsep_box.add_widget(slot__kataasing)
 
         box.add_widget(konsep_box)
-        # box.add_widget(MDLabel(text='\n'.join([f'{i}. {x.keterangan}' for i, x in enumerate(self.lemma.konsep, 1)])))
         self.add_widget(box)
         return self
 
     def on_lemma(self, instance, value: LemmaData) -> None:
-        # self.lemma_nama = value.nama
-        # self.lemma_konsep = '\n'.join([f'{i}. {x.keterangan}' for i, x in enumerate(value.konsep, 1)])
-        # self.lemma_golongan_kata = value.konsep[0].golongan.id
-        # self.lemma_cakupan = value.konsep[0].cakupan.__repr__()
         self.build()
 
 
